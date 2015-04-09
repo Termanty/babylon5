46058734a5f3887ab62c9fb4729492b815c2d796
Johannes Elmnäinen <sanhologin@gmail.com>
2015-04-09 16:16:19 +0300

    initial commit

---
+++ src/main/java/com/babylon5/bibtextarchive/main.java
@@ -0,0 +1,15 @@
+
+package com.babylon5.bibtextarchive;
+
+import IO.ConsoleIO;
+import java.util.Scanner;
+
+public class main {
+    
+    public static void main(String[] args){
+        ConsoleIO io = new ConsoleIO();
+        io.start();
+    }
+
+    
+}
