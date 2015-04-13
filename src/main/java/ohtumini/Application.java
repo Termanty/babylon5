0df2cfe78464d0d1ebdead90a5c55a9e23755a71
Termanty <termanty@cs.helsinki.fi>
2015-04-13 10:55:12 +0300

    Profiles development and production added

--- src/main/java/ohtumini/Application.java
+++ src/main/java/ohtumini/Application.java
@@ -1,15 +1,19 @@
 
 package ohtumini;
 
+import ohtumini.profiles.DevProfile;
+import ohtumini.profiles.ProdProfile;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
  
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
+@Import({DevProfile.class, ProdProfile.class})
 public class Application {
  
     public static void main(String[] args) {
