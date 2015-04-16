670d8f2f2414b55b401b4f645125b7846a5f0c1d
Johannes Elmnäinen <jelmnain@cs.helsinki.fi>
2015-04-16 18:11:39 +0300

    hotfix

--- src/main/java/ohtumini/repository/ReferenceRepository.java
+++ src/main/java/ohtumini/repository/ReferenceRepository.java
@@ -2,14 +2,14 @@
 package ohtumini.repository;
 
 import ohtumini.domain.Article;
-import ohtumini.domain.Book;
+//import ohtumini.domain.Book;
 import ohtumini.domain.Reference;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReferenceRepository extends JpaRepository<Reference, Long> {
 
     public Reference findByTitle(String title);
-    public Book findByEditor(String editor);
+    //public Book findByEditor(String editor);
     
     // custom queries to db will be writen here
     
