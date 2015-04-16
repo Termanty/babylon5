e17a89e9dbb69c7a71c1ad514f00e150bbd71827
Parvamo Mikael A <maparvam@da1-cs-bk107-03.helsinki.fi>
2015-04-16 15:35:11 +0300

    All tests working and testing all the current fields

--- src/test/java/ohtumini/AddingNewReferencesToDbTest.java
+++ src/test/java/ohtumini/AddingNewReferencesToDbTest.java
@@ -30,15 +30,25 @@ public class AddingNewReferencesToDbTest {
         article.setNumber("1");
         article.setPubKey("key");
         article.setJournal("sadfdas");
+        article.setNote("note1");
         
         refRepo.save(article);
         
         Reference retrieved = refRepo.findByTitle("Juhuu");
         assertNotNull(retrieved);
-        assertEquals("mina", retrieved.getAuthor());
         assertEquals("ARTICLE", retrieved.getReferenceType());
+        
         Article retri = (Article)retrieved;
+        
+        assertEquals("mina", retri.getAuthor());
+        assertEquals("Juhuu", retri.getTitle());
+        assertEquals("2015", retri.getPubYear());
+        assertEquals("May", retri.getPubMonth());
         assertEquals("14", retri.getPages());
+        assertEquals("1", retri.getNumber());
+        assertEquals("key", retri.getPubKey());
+        assertEquals("sadfdas", retri.getJournal());
+        assertEquals("note1", retri.getNote());
     }
     
     @Test
@@ -50,15 +60,24 @@ public class AddingNewReferencesToDbTest {
         book.setPubKey("key2");
         book.setEditor("Editer");
         book.setPublisher("mina");
-        
-        
+        book.setNote("note2");
+            
         refRepo.save(book);
         
         Reference retrieved = refRepo.findByTitle("Book1");
         assertNotNull(retrieved);
-        assertEquals("Tolkien", retrieved.getAuthor());
         assertEquals("BOOK", retrieved.getReferenceType());
-        assertEquals("Book1", retrieved.getTitle());
+        
+        Book retri = (Book)retrieved;
+        
+        assertEquals("Tolkien", retri.getAuthor());
+        assertEquals("Book1", retri.getTitle());
+        assertEquals("2015", retri.getPubYear());
+        assertEquals("key2", retri.getPubKey());
+        assertEquals("Editer", retri.getEditor());
+        assertEquals("mina", retri.getPublisher());
+        assertEquals("note2", retri.getNote());
+        
     }
     
     @Test
@@ -69,6 +88,27 @@ public class AddingNewReferencesToDbTest {
         inproceedings.setPubYear("1876");
         inproceedings.setPubKey("key3");
         inproceedings.setNote("Note3");
+        inproceedings.setBookTitle("All About Dance");
+        inproceedings.setPubMonth("March");
+        inproceedings.setOrganisation("DanceCorp");
+        
+        refRepo.save(inproceedings);
+        
+        Reference retrieved = refRepo.findByTitle("How To Dance Without Legs");
+        assertNotNull(retrieved);
+        assertEquals("INPROCEEDINGS", retrieved.getReferenceType());
+        
+        Inproceedings retri = (Inproceedings) retrieved;
+        
+        assertEquals("How To Dance Without Legs", retri.getTitle());
+        assertEquals("Leglez", retri.getAuthor());
+        assertEquals("1876", retri.getPubYear());
+        assertEquals("key3", retri.getPubKey());
+        assertEquals("Note3", retri.getNote());
+        assertEquals("All About Dance", retri.getBookTitle());
+        assertEquals("March", retri.getPubMonth());
+        assertEquals("DanceCorp", retri.getOrganisation());
+                
     }
 
 }
