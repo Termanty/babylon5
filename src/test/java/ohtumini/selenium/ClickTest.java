9a6f22e025c061a8f117804a517aabecd950a2a5
Mäntylä Tero K <termanty@da3-cs-dk110-03.helsinki.fi>
2015-04-17 10:11:43 +0300

    First selenium test

---
+++ src/test/java/ohtumini/selenium/ClickTest.java
@@ -0,0 +1,33 @@
+
+package ohtumini.selenium;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+
+public class ClickTest {
+    
+    private WebDriver driver;
+    
+    @Before
+    public void setUp() {
+        this.driver = new HtmlUnitDriver();
+    }
+    
+    
+    @Test
+    public void resourcesPageLinksWorkTest() {
+        driver.get("http://localhost:8080/references");
+        System.out.println(driver.getPageSource());
+        
+        WebElement element = driver.findElement(By.linkText("newReference"));   
+        element.click();
+        Assert.assertTrue(driver.getPageSource().contains("Choose Reference Type to create"));
+    }
+}
