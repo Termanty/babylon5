ab5346c411ab9b5d6accdebe69c8394e02f04c52
Mäntylä Tero K <termanty@da3-cs-dk110-03.helsinki.fi>
2015-04-17 14:45:44 +0300

    MAJOR: Added selenium tests to run with tomcat integration-test phase

--- src/main/java/ohtumini/Application.java
+++ src/main/java/ohtumini/Application.java
@@ -5,6 +5,9 @@ import ohtumini.profiles.DevProfile;
 import ohtumini.profiles.ProdProfile;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -14,8 +17,14 @@ import org.springframework.context.annotation.Import;
 @EnableAutoConfiguration
 @ComponentScan
 @Import({DevProfile.class, ProdProfile.class})
-public class Application {
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
  
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    } 
+    
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
